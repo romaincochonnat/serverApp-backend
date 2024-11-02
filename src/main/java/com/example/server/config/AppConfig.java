f27434245c9e199626ae541ecfb6328b0875a46f
Romain Cochonnat <romain.cochonnat@gmail.com>
2024-11-02 18:38:57 +0100

    test config profil

--- src/main/java/com/example/server/config/AppConfig.java
+++ src/main/java/com/example/server/config/AppConfig.java
@@ -2,10 +2,12 @@ package com.example.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.FileSystemResource;
 
 @Configuration
+@Profile("local") // This class will only be loaded when the "local" profile is active
 public class AppConfig {
 
     @Bean
